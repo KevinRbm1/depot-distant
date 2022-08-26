2b23efc54bd0c2215b7ec26b09f6b89ac915fbf0
KevinRbm1 <ruizk7993@gmail.com>
2022-08-26 10:05:44 +0200

    update fix

--- velvet record/php/forgotpassword.php
+++ velvet record/php/forgotpassword.php
@@ -5,26 +5,26 @@
 ?>
 <div class="container mt-5">
     <form action="script_login.php" method="POST">
-        <div class="alert alert-primary" role="alert">
+        <div class="alert alert-info" role="alert">
             <h4 class="alert-heading text-center">Voici le formulaire de changement de mot de passe.</h4>
             <br>
             <p class="text-center">Pour votre sécurité, veuillez saisir un mot de passe sûre.</p>
             <hr>
-            <p class="mb-0">Un mot de passe sûre doit contenir :<br>
-            - au moins dix caractères ;<br>
-            - une letre majuscule ;<br>
-            - un chiffre ;<br>
-            - et un caractère spécial.</p>
+            <p class="mb-0">Un mot de passe sûre doit contenir les critères suivants:<br><br>
+            - Au moins dix caractères ;<br>
+            - Une lettre majuscule ;<br>
+            - Chiffre(s) ;<br>
+            - Caractère(s) spécial(es).</p>
         </div>
         <p class="text-center mt-5 mb-5 text-danger"></p>
         <div class="container d-flex">
             <div class="input-group mt-5 mx-3 mb-5">
-                <span class="input-group-text bg-dark text-light" id="basic-addon3"  title="Nouveau mote de passe">Nouveau</span>
+                <span class="input-group-text bg-warning text-light" id="basic-addon3"  title="Nouveau mote de passe">Nouveau</span>
                 <input type="text" class="form-control" id="password" name="password" placeholder="Veuillez saisir le nouveau mot de passe" value="" aria-describedby="basic-addon3"  title="Saisissez le nouveau mot de passe" require>
                 <p class="comments"> </p>
             </div>
             <div class="input-group mt-5 mx-3 mb-5">
-                <span class="input-group-text bg-dark text-light rounded" id="basic-addon3" title="Comfirmer le mot de passe">Confirmer</span>
+                <span class="input-group-text bg-danger text-light rounded" id="basic-addon3" title="Comfirmer le mot de passe">Confirmer</span>
                 <input type="password" class="form-control rounded" id="password" name="password" placeholder="Veuillez confimer le mot de passe"  title="Ressaisissez le mot de passe" value="" aria-describedby="basic-addon3" require>
                 <p class="comments"> </p>
             </div>
